ccf288451c86d16d9d2a6155e9beda173fc93f6e
Dipak Warade <dipakvwarade@users.noreply.github.com>
2021-09-06 17:54:09 +0300

    Update create_api_calls.py

--- create_api_calls.py
+++ create_api_calls.py
@@ -13,6 +13,7 @@ __copyright__ = "Dipak Warade <dipakvwarade@gmail.com>"
 import logging
 from logging.handlers import TimedRotatingFileHandler
 import pandas as pd
+from jinja2 import Template
 
 
 def xls_to_dict(underlay_data_dic,counter):
@@ -35,13 +36,26 @@ def xls_to_dict(underlay_data_dic,counter):
 #     "network_name": "dipak_sriov",
 #     "project_name": "vIMS_Project" }
 
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+
 def main(): 
+  
+  file_name = "api_calls.txt"
+  logHandler = TimedRotatingFileHandler(file_name,when="M",interval=10, backupCount=1000000)
+  logger = logging.getLogger( 'MyLogger' )
+  logger.addHandler( logHandler )
+  logger.setLevel( logging.INFO )
+  
+  j2_template = Template(open('api_template.j2').read())
   underlay_data_dic = pd.read_excel('sriov_vlan_push_template.xlsx').to_dict()
   for i in range(len(underlay_data_dic["Logical_Network"])):
     counter = i
     f_data = xls_to_dict(underlay_data_dic,counter)
-    
-    
+    api_call = j2_template.render(f_data)
+    logger.info(api_call)
     
   
 
